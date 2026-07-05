080279bb4125910548f219f7d3fa841131224623
Kevin <pinnaclestudios4u@gmail.com>
2026-07-05 23:26:52 +0530

    Center align the Holistic box on the second row of the Clinical Philosophy grid in About page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -220,11 +220,7 @@ export default function AboutPage() {
               </p>
             </div>
 
-            <div style={{
-              display: 'grid',
-              gridTemplateColumns: 'repeat(auto-fit, minmax(300px, 1fr))',
-              gap: '2rem'
-            }}>
+            <div className="pillars-grid">
               <motion.div 
                 className="glass rounded-l" 
                 style={{ 
